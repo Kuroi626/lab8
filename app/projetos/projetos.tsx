16478efa83af98ef08f5e2119b6f0be3e3c44bb5
Manuel António Baptista Dos Santos <74881553+Kuroi626@users.noreply.github.com>
2026-01-09 13:31:28 +0000

    sup

--- app/projetos/projetos.tsx
+++ app/projetos/projetos.tsx
@@ -6,7 +6,7 @@ export default function ProjetosPage() {
 return (
 <main className="p-8">
 <h2 className="text-2xl font-bold mb-6">Projetos</h2>
-<DescricaoProjetos />
+<DescricaoProjetos/>
 </main>
 );
 }
